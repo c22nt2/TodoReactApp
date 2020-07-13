627e8c30eedad21167053e1cb47e526e3f218feb
Thuong DV <c22nt2@gmail.com>
2020-07-13 11:42:51 +0700

    Updates

---
+++ precache-manifest.9cf78fccc3c37a5abb035cf35a236505.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9fb97511673cfe8b082df6853dd3edb",
+    "url": "/TodoReactApp/index.html"
+  },
+  {
+    "revision": "7d4b0f98d3a2c0f273dd",
+    "url": "/TodoReactApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c3d3f60d675400b269b5",
+    "url": "/TodoReactApp/static/js/2.a0ce437b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoReactApp/static/js/2.a0ce437b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d4b0f98d3a2c0f273dd",
+    "url": "/TodoReactApp/static/js/main.33b3d901.chunk.js"
+  },
+  {
+    "revision": "ab96d7bd9908d603ae0c",
+    "url": "/TodoReactApp/static/js/runtime-main.7590630c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TodoReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
